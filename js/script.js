38264854e8aa624a9b6250a2bc50430dde488b1a
YitengZhang <yiteng@umich.edu>
2026-04-21 16:29:19 -0700

    final version

--- js/script.js
+++ js/script.js
@@ -1,10 +1,3 @@
-const addNumbers = (first, second) => {
-  return first + second;
-}
-
-if (typeof module !== 'undefined') {
-  module.exports = {
-    addNumbers
-  }
-}
-
+document.querySelector('#button').addEventListener('click', () => {
+  document.querySelector('#output').textContent = 'Button clicked';
+});
